81a8debfb3b28f31560edbe4ddb5d798d2895d40
MushroomovAA <nynblpka@gmail.com>
2022-04-17 22:21:06 +0300

    !

--- Task_6_1.py
+++
@@ -1,7 +0,0 @@
-file = open('logs.txt')
-
-tuplec = []
-for line in file:
-    splitted = line.split()
-    tuplec.append((splitted[0], splitted[5].replace('"', ''), splitted[6]))
-print('\n'.join(map(str, tuplec)))
---
+++ Task_7_2.py
@@ -0,0 +1,34 @@
+import yaml
+import os
+
+dic = {'my_project':
+    [{'settings': ['__init__.py', 'dev.py', 'prod.py'], },
+    {'mainapp': ['__init__.py', 'models.py', 'views.py', {'templates': [{'mainapp': ['base.html', 'index.html']}]}]},
+    {'authapp': ['__init__.py', 'models.py', 'views.py', {'templates': [{'authapp': ['base.html', 'index.html']}]}]}]}
+
+funk = open('config.yaml', 'w')
+funk.write(yaml.dump(dic))
+funk.close()
+
+with open('config.yaml') as f_yaml:
+    dic = yaml.safe_load(f_yaml)
+
+
+def create_data(data):
+    for folder, data_tmps in data.items():
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        os.chdir(folder)
+        for data_tmp in data_tmps:
+            if isinstance(data_tmp, dict):
+                create_data(data_tmp)
+            else:
+                if not os.path.exists(data_tmp):
+                    if '.' in data_tmp:
+                        with open(data_tmp, 'w') as f:
+                            f.write('')
+    else:
+        os.chdir('../')
+
+
+create_data(dic)
